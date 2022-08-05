083bae936107ccab63dca5d56a4a999e6e4b72b9
skclsso25 <skclsso52@naver.com>
2022-08-05 15:13:28 +0900

    01_javaproject
    새로운 거 추가

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,27 @@
+package edu.kh.test;
+
+// 한 줄 주석 (컴파일러(번역기)가 해석하지 않는 부분)
+
+/* 범위주석 */
+
+// class : 자바 코드가 작성되는 영역
+public class JavaTest {
+
+	/* main 메서드 : 자바(애플리케이션(프로그램)을 실행하기 위해서 반드시 필요한 구문 
+	   {로 시작해서 }로 끝나는 부분까지   */
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World~");
+		System.out.println("내 점심은 오므라이스 뿌잉뿌잉");
+		System.out.println("println()은 한줄 출력입니다.");
+		System.out.println( 100 + 200 + 300 + 400 );
+		System.out.println("식곤증...");
+		
+		/* 변수 : 메모리(RAM)에 값을 기록하기 위한 공간
+		 * (*변수의 값은 변할 수 있다.)
+		 * 변수의 선언 : 실수형 / 정수형/ 논리형 / 문자형 / 문자열 
+		 * */
+		
+	}
+
+}
